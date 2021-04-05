1efa516f19124dbb5b2b1d0fe106ea681c1a0098
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-04-05 13:10:38 -0400

    binary tree level

---
+++ Binary_Tree_Level_order.js
@@ -0,0 +1,75 @@
+// Level-order traversal is to traverse the tree level by level.
+
+// Breadth-First Search is an algorithm to traverse or search in data structures 
+// like a tree or a graph. The algorithm starts with a root node and visit the node 
+// itself first. Then traverse its neighbors, traverse its second level neighbors, 
+// traverse its third level neighbors, so on and so forth.
+
+
+
+//  Binary Tree Level Order Traversal
+
+//  Solution
+//  Given the root of a binary tree, return the level order traversal of its nodes' 
+//  values. (i.e., from left to right, level by level).
+
+ 
+//  Example 1:
+//  Input: root = [3,9,20,null,null,15,7]
+//  Output: [[3],[9,20],[15,7]]
+
+//  Example 2:
+//  Input: root = [1]
+//  Output: [[1]]
+
+//  Example 3:
+//  Input: root = []
+//  Output: []
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if(!root) return [];
+    
+    let result = [];
+    let queue = [root];
+    
+    while(queue.length) {
+        //set a new variable instead of using (let i=0; i<queue.length; i++) 
+        //queue length will constantly changed when we push the left and right node 
+        let queueLength = queue.length;
+        let currentLevel = [];
+        
+        //two loop, the queue loop, and the currentLevel loop
+        //the queueLength will be no more than 2, left node and right node
+        for (let i=0; i<queueLength; i++) {
+            //mutating the original array, reducing the length
+            //set the current node, starting from the left
+            let node = queue.shift();
+            
+            //push the current node val
+            currentLevel.push(node.val);
+            
+            //push the left and right node into the queue if they exist
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        
+        //when finished with currentLevel, push then reset 
+        result.push(currentLevel)
+    }
+    return result;
+};
