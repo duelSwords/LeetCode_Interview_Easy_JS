accecc4c7ba79e67d0db3cb993d6dcf302451da4
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-06-06 16:27:45 -0400

    TTP tree

--- TTP.js
+++ TTP.js
@@ -1,4 +1,12 @@
 
+// https://codeinterview.io/playback/CTKZRMJCCM#?t=917
+
+// https://codeinterview.io/playback/KMIQTKFVUR#?t=3
+
+// https://codeinterview.io/playback/DDUDCLDAQD#?t=576
+
+
+
 
 
 //////////////////////////////////
@@ -77,3 +85,76 @@ function isCircular(node) {
     return false
 }
 
+
+
+
+////////////////////
+///// Tree /////////
+////////////////////
+
+
+// 1. Using a queue and while loop write a function that takes the root of a
+// binary tree node and outputs an array of values ordered by BREADTH FIRST.
+
+// BREADTH FIRST traversal: [4,2,5,1,3,7,6,8]
+// */
+
+function bfs(node) {
+  let results =[]
+  let queue = [node]
+    while(queue.length){
+      let currentNode = queue.shift()
+      results.push(currentNode.value)
+      if(currentNode.left) queue.push(currentNode.left)
+      if(currentNode.right) queue.push(currentNode.right)
+    }
+    return results
+}
+
+
+
+function dfsPre(node) {
+    let result = []
+    
+    let preorder = node => {   
+      if(node === null) return
+      result.push(node.value)
+      if(node.left) preorder(node.left)
+      if(node.right) preorder(node.right)
+    }
+    preorder(node)
+  
+    return result
+  }
+
+
+  function dfsIn(node) {
+    let result = []
+    
+    let inorder = node => {   
+      if(node === null) return
+      if(node.left) inorder(node.left)
+      result.push(node.value)
+      if(node.right) inorder(node.right)
+    }
+    inorder(node)
+  
+    return result
+  }
+  
+  
+
+
+  function dfsPost(node) {
+    let result = []
+    
+    let postorder = node => {   
+      if(node === null) return
+      if(node.left) postorder(node.left)
+      if(node.right) postorder(node.right)
+      result.push(node.value)
+    }
+    postorder(node)
+  
+    return result
+  }  
