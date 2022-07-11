0a86cfdf56d9f641558c0b9252cce14d55af1975
Dong-Yi-Xia <dong7777xia@gmail.com>
2022-07-11 19:08:14 -0400

    sort LL

--- Easy/Linked_list_merge_two_sorted.js
+++ Easy/Linked_list_merge_two_sorted.js
@@ -83,3 +83,26 @@ var mergeTwoLists = function(list1, list2) {
     
     return result.next
 };
+
+
+var mergeTwoLists = function(list1, list2) {
+    
+    let dummy = new ListNode
+    let tail = dummy
+    
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            tail.next = list1
+            list1 = list1.next
+        }else {
+            tail.next = list2
+            list2 = list2.next
+        }
+        tail = tail.next
+    }
+    
+    tail.next = list1 || list2
+    
+    return dummy.next
+    
+};
